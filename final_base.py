3391a0600b0c6395507e52c2c1943f44bb89fd82
Said Aghamaliyev <digitalwarior666@gmail.com>
2026-05-01 20:57:46 +0400

    Update final_base.py

--- final_base.py
+++ final_base.py
@@ -1,1 +1,29 @@
+import sqlite3 as sq
+import datetime as dt
 
+def Base_Write(count):
+    connection = sq.connect("c:/Users/User/Desktop/Novum/novum-app-main/src/bread.db")
+    cursor = connection.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS bread_sales(
+                    Id INTEGER PRIMARY KEY,
+                    Day INTEGER,
+                    Month INTEGER,
+                    Year INTEGER,
+                    Amount INTEGER)
+        ''')
+    cursor.execute("SELECT MAX(Id) FROM bread_sales")
+    result = cursor.fetchone()[0]
+    if result is None:
+        result = 0
+    Id = result + 1
+    Amount = count
+
+    Today = dt.date.today()
+    Day = Today.day
+    Month = Today.month
+    Year = Today.year
+    cursor.execute("INSERT INTO bread_sales (Id,Amount,Day,Month,Year) VALUES (?,?,?,?,?)",(Id,Amount,Day,Month,Year))
+    connection.commit()
+    connection.close()
+    
